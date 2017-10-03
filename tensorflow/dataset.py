05faf6c3b10a80c6254941090744110b67c78734
Zheng Fu <zf38@cornell.edu>
2017-10-03 14:12:49 -0400

    finished dataset.py

--- tensorflow/dataset.py
+++ tensorflow/dataset.py
@@ -46,9 +46,72 @@ class DataSet(object):
         self._images = images
         self._labels = labels
         self._img_names = img_names
-        self.cls = cls
+        self._cls = cls
         self._epochs_done = 0
         self._index_in_epoch = 0
 
     @property
     def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def img_names(self):
+        return self._img_names
+
+    @property
+    def cls(self):
+        return self._cls
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_done(self):
+        return self._epochs_done
+
+    def next_batch(self, batch_size):
+        """Return the next 'batch_size' examples from this data set."""
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+
+        if self._index_in_epoch > self._num_examples:
+            # Updates after each epoch
+            self._epochs_done += 1
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+
+        return self._images[start:end], self._labels[start:end], self._img_names[start:end], self._cls[start:end]
+
+
+def read_training_dataset(data_path, image_size, classes, validation_size):
+    class DataSets(object):
+        pass
+    data_sets = DataSets()
+
+    images, labels, img_names, cls = load_training_data(data_path, image_size, classes)
+    images, labels, img_names, cls = shuffle(images, labels, img_names, cls)
+
+    if isinstance(validation_size, float):
+        validation_size = int(validation_size * images.shape[0])
+
+    validation_images = images[:validation_size]
+    validation_labels = labels[:validation_size]
+    validation_img_names = img_names[:validation_size]
+    validation_cls = cls[:validation_size]
+
+    training_images = images[validation_size:]
+    training_labels = labels[validation_size:]
+    training_img_names = img_names[validation_size:]
+    training_cls = cls[validation_size:]
+
+    data_sets.train = DataSet(training_images, training_labels, training_img_names, training_cls)
+    data_sets.valid = DataSet(validation_images, validation_labels, validation_img_names, validation_cls)
+
+    return data_sets
